6c22154cef6b855cccdb9c6821b187694601ec15
Bhavani Prakash <bhavaniprakash960@gmail.com>
2026-02-12 22:42:07 +0530

    greeting changed

--- src/pages/Greeting.jsx
+++ src/pages/Greeting.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Greeting = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>Thank you for registration!</h2>
-      <p>Your data was submitted successfully.</p>
+      <h2>Thank you for registration! at ECLECTICA 2K26</h2>
+      
       <br />
-      <p>Check your mail for confirmation. <br/><b>NOTE: check your spam folder if you don't see the email.</b> </p>
+      <p>Check your mail for confirmation. <br/><br></br><b>NOTE: check your spam folder if you don't see the email.</b> </p>
     </div>
   );
 };
